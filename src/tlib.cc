c9dfe0bfa2e8d1f2de81ab513ee9ab0a990858c7
Jatin Chaudhary <Jatin.Chaudhary@amd.com>
2019-12-13 13:16:46 +0530

    Reverting opti patch

--- src/tlib.cc
+++ src/tlib.cc
@@ -1,8 +1,7 @@
 #include "tlib.h"
 
-void vecAdd(std::vector<int> &a, std::vector<int> &b, std::vector<int> &c) {
+void vecAdd(std::vector<int> a, std::vector<int> b, std::vector<int> &c) {
   // c = a+b;
-  c.reserve(a.size());
   for (int i = 0; i < a.size(); i++) {
     c.push_back(a[i] + b[i]);
   }
