e40ca4d6bd65e187eb5bf305b2bcd8741fa763e1
Jatin Chaudhary <jachaudh@amd.com>
2019-12-15 14:49:05 +0530

    Adding flaot 2,3,4 test

---
+++ test/hipFloat3.h
@@ -0,0 +1,47 @@
+#include "hipVectors.h"
+#include <benchmark/benchmark.h>
+#include <hip/hip_runtime.h>
+
+static void hipFloat3BenchAdd(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        float3 a;
+        benchFloat3Add(a, 300, 100);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(hipFloat3BenchAdd);
+
+static void hipFloat3BenchMul(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        float3 a;
+        benchFloat3Mul(a, 300, 100);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(hipFloat3BenchMul);
+
+static void hipFloat3BenchDiv(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        float3 a;
+        benchFloat3Add(a, 300, 100);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(hipFloat3BenchDiv);
+
+static void hipFloat3BenchSub(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        float3 a;
+        benchFloat3Mul(a, 300, 100);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(hipFloat3BenchSub);
---
+++ test/hipFloat4.h
@@ -0,0 +1,47 @@
+#include "hipVectors.h"
+#include <benchmark/benchmark.h>
+#include <hip/hip_runtime.h>
+
+static void hipFloat4BenchAdd(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        float4 a;
+        benchFloat4Add(a, 300, 100);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(hipFloat4BenchAdd);
+
+static void hipFloat4BenchMul(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        float4 a;
+        benchFloat4Mul(a, 300, 100);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(hipFloat4BenchMul);
+
+static void hipFloat4BenchDiv(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        float4 a;
+        benchFloat4Add(a, 300, 100);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(hipFloat4BenchDiv);
+
+static void hipFloat4BenchSub(benchmark::State& state) {
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        float4 a;
+        benchFloat4Mul(a, 300, 100);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(hipFloat4BenchSub);
